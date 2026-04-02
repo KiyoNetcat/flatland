fcb5d665c615de1ff99cf9d692ce81018454ecd0
Kiyo <kiyo@ncat.cafe>
2026-04-02 01:46:28 -0700

    update flake, remove unused res prefix

--- flake.nix
+++ flake.nix
@@ -17,13 +17,6 @@
         src = ./.;
 
         cargoTestCommand = "echo \"looks good to me\"";
-        
-        STARDUST_RES_PREFIXES = pkgs.stdenvNoCC.mkDerivation {
-          name = "resources";
-          src = ./.;
-  
-          buildPhase = "cp -r $src/res $out";
-        };
       };
     });
   };
